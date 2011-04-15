394f05cd7a21fa96cf085ae0ff46a9a3abab98f9
trane <trane@xmission.com>
2011-04-14 20:18:40 -0600

    Add CSMessage data model

---
+++ Chatosaurus/Chatosaurus/Classes/CSMessage.h
@@ -0,0 +1,14 @@
+//
+//  CSMessage.h
+//  Chatosaurus
+//
+//  Created by trane on 4/14/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+@interface CSMessage : NSObject 
+{
+    
+}
+
+@end
---
+++ Chatosaurus/Chatosaurus/Classes/CSMessage.m
@@ -0,0 +1,40 @@
+//
+//  CSMessage.m
+//  Chatosaurus
+//
+//  Created by trane on 4/14/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+#import "CSMessage.h"
+
+#pragma mark -
+#pragma mark Private Interface
+@interface CSMessage ()
+@end
+
+#pragma mark -
+@implementation CSMessage
+
+#pragma mark Constructors
+- (id) init
+{
+    self = [super init];
+    if (self == nil)
+        return nil;
+    
+    return self;
+}
+
+- (void) dealloc
+{
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+#pragma mark -
+#pragma mark Methods
+
+@end

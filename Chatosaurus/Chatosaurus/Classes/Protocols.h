a8312a709f9d08df1cc18ca543f892956fe1ca73
trane <trane@xmission.com>
2011-04-14 21:00:54 -0600

    Create file to hold enum of available protocols

---
+++ Chatosaurus/Chatosaurus/Classes/Protocols.h
@@ -0,0 +1,17 @@
+//
+//  Protocols.h
+//  Chatosaurus
+//
+//  Created by trane on 4/14/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+/**
+ * Gives the available protocols
+ */
+@interface Protocols : NSObject 
+{
+    
+}
+
+@end
---
+++ Chatosaurus/Chatosaurus/Classes/Protocols.m
@@ -0,0 +1,40 @@
+//
+//  Protocols.m
+//  Chatosaurus
+//
+//  Created by trane on 4/14/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+#import "Protocols.h"
+
+#pragma mark -
+#pragma mark Private Interface
+@interface Protocols ()
+@end
+
+#pragma mark -
+@implementation Protocols
+
+#pragma mark Constructors
+- (id) init
+{
+    self = [super init];
+    if (self == nil)
+        return nil;
+    
+    return self;
+}
+
+- (void) dealloc
+{
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+#pragma mark -
+#pragma mark Methods
+
+@end

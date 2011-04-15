b110347e1b46742e176c9d4bcedd26ee20bd7a1c
trane <trane@xmission.com>
2011-04-14 20:16:43 -0600

    Add channel grid view components

---
+++ Chatosaurus/Chatosaurus/Classes/CSChannelView.m
@@ -0,0 +1,45 @@
+//
+//  CSChannelView.m
+//  Chatosaurus
+//
+//  Created by trane on 4/14/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+#import "CSChannelView.h"
+
+#pragma mark -
+#pragma mark Private Interface
+@interface CSChannelView ()
+@end
+
+#pragma mark -
+@implementation CSChannelView
+
+#pragma mark Constructors
+- (id) initWithFrame:(CGRect)frame 
+{
+    self = [super initWithFrame:frame];
+	if (self == nil)
+		return nil;
+	
+    return self;
+}
+
+- (void) dealloc 
+{
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+#pragma mark -
+#pragma mark Methods
+
+#pragma mark UIView Methods
+- (void) layoutSubviews
+{
+}
+
+@end
---
+++ Chatosaurus/Chatosaurus/Classes/CSChannelViewCell.m
@@ -0,0 +1,45 @@
+//
+//  CSChannelViewCell.m
+//  Chatosaurus
+//
+//  Created by trane on 4/14/11.
+//  Copyright 2011 home. All rights reserved.
+//
+
+#import "CSChannelViewCell.h"
+
+#pragma mark -
+#pragma mark Private Interface
+@interface CSChannelViewCell ()
+@end
+
+#pragma mark -
+@implementation CSChannelViewCell
+
+#pragma mark Constructors
+- (id) initWithFrame:(CGRect)frame 
+{
+    self = [super initWithFrame:frame];
+	if (self == nil)
+		return nil;
+	
+    return self;
+}
+
+- (void) dealloc 
+{
+    [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+#pragma mark -
+#pragma mark Methods
+
+#pragma mark UIView Methods
+- (void) layoutSubviews
+{
+}
+
+@end

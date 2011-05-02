db2f489f364a2059c3e2ba699d3524db49eccbef
trane <trane@xmission.com>
2011-05-02 14:13:13 -0600

    Implement MessageCollection data structure

--- Chatosaurus/Chatosaurus/Classes/CSMessageCollection.h
+++ Chatosaurus/Chatosaurus/Classes/CSMessageCollection.h
@@ -20,6 +20,8 @@
 @interface CSMessageCollection : NSObject 
 {
     NSObject<CSMessageCollectionDelegate> *_delegate;
+
+    NSMutableDictionary *_origins;
 }
 
 /*!
@@ -27,4 +29,13 @@
  */
 @property (nonatomic,assign) NSObject<CSMessageCollectionDelegate> *delegate;
 
+@property (nonatomic,assign) NSMutableDictionary *origins;
+
+/*!
+ * This will populate the origins dictionary with a:
+ * Servers dictionary that contains a dictionary of channels of that server
+ * Each channel dictionary contains an empty array that will be populated
+ * with incoming messages to that array
+ */
+- (void) addServers:(NSArray*)servers;
 @end
--- Chatosaurus/Chatosaurus/Classes/CSMessageCollection.m
+++ Chatosaurus/Chatosaurus/Classes/CSMessageCollection.m
@@ -23,11 +23,13 @@
     if (self == nil)
         return nil;
     
+    _origins = [[NSMutableDictionary alloc] init];
     return self;
 }
 
 - (void) dealloc
 {
+    [_origins release];
     [super dealloc];
 }
 
@@ -35,7 +37,34 @@
 #pragma mark Accessors
 @synthesize delegate = _delegate;
 
+@synthesize origins = _origins;
+- (void) setOrigins:(NSMutableDictionary *)origins
+{
+    _origins = origins;
+}
 #pragma mark -
 #pragma mark Methods
+- (void) addServers:(NSArray *)servers
+{
+    for (int i = 0; i < [servers count]; i++) {
 
+        // Grab the server
+        NSDictionary *dict = [servers objectAtIndex:i];
+
+        // Get the server name
+        NSString *serverName = [dict objectForKey:@"serverName"];
+        
+        // Create the channel dictionaries
+        NSArray *channels = [[NSArray arrayWithArray:[dict objectForKey:@"channels"]] retain];
+        NSMutableDictionary *channelNames = [[[NSMutableDictionary alloc] init] autorelease];
+        for (int j = 0; j < [channels count]; j++) {
+            NSMutableArray *channelMessages = [[[NSMutableArray alloc] init] autorelease];
+            
+            [channelNames setValue:channelMessages forKey:[[channels objectAtIndex:j] valueForKey:@"name"]];
+        }
+        
+        // Create the Origins Server->Channels dictionary
+        [_origins setValue:channelNames forKey:serverName];
+    }
+}
 @end
